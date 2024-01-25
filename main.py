cba22b719edf4ed70bb1b3047c9c753eb77ad4f0
Lucas Daniel <lucasdaniel.geo@gmail.com>
2024-01-25 18:48:50 -0300

    refactor: modularize and simplify post scraping
    - Extracted functions for better code isolation
    - Renamed functions for clarity and consistency
    - Improved typing annotations for enhanced readability

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
+from typing import List
+
 import requests
 from bs4 import BeautifulSoup
+from requests import Response
+
+
+def request_page_html(url: str) -> Response:
+    response = requests.get(url)
+
+    return response.text
+
+
+def extract_post_list_items(soup: BeautifulSoup) -> List[str]:
+    return soup.div.div.main.ol.contents
+
+
+def extract_post_title(li: str) -> str:
+    title = li.article.div.a.text
+
+    return title if title else ""
+
+
+def extract_post_titles(posts: List[str]) -> List[str]:
+    return [extract_post_title(li) for li in posts]
 
 
 def main():
-    print("Starting scraping...")
-
     base_url = "https://www.tabnews.com.br/recentes/pagina/"
-    response = requests.get(f"{base_url}1")
-    html = response.text
+    page = 1
+    html_text = request_page_html(f"{base_url}{page}")
 
-    soup = BeautifulSoup(html, features="html.parser")
+    soup = BeautifulSoup(html_text, features="html.parser")
 
-    # __next > div > div > main > ol > li:nth-child(1) > article > div.Box-sc-g0xbh4-0.ivqXmt > a
-    ol = soup.div.div.main.ol.contents
+    post_items = extract_post_list_items(soup=soup)
+    post_titles = extract_post_titles(posts=post_items)
 
-
-    print("[INFO] Printing post names from TabNews first page...")
-    for li in ol:
-        print(li.article.div.a.text)
+    print("[INFO] Printing post names from first page...\n")
+    for title in post_titles:
+        print(title)
 
 
 if __name__ == "__main__":
